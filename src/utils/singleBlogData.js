8aee25d4bdd2f4a77cc0186d40db29b0bb2a761f
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-08 12:39:56 +0600

    server side rendering for no-cache

--- src/utils/singleBlogData.js
+++ src/utils/singleBlogData.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const singleBlogData = async (id) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-cache",
+  });
   return res.json();
   return <div></div>;
 };
