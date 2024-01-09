5bafe9a62c0db3c554fb0e6ebb824ccbd692a8cb
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-10 00:49:34 +0600

    up

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,10 +1,18 @@
+import PopularProduct from "@/components/HomePage/PopularProduct";
 import Categories from "@/components/HomePage/categories";
-
+import { Suspense } from "react";
+export const revalidate = 5;
 export default function Home() {
   return (
     <main>
-      <Categories/>
-  
+      <Categories />
+      <Suspense
+        fallback={
+          <h1 className="text-center font-semibold text-2xl">Loading..</h1>
+        }
+      >
+        <PopularProduct />
+      </Suspense>
     </main>
   );
 }
--- src/components/HomePage/PopularProduct.jsx
+++ src/components/HomePage/PopularProduct.jsx
@@ -1,9 +1,10 @@
+import wait from "@/utils/wait";
 import React from "react";
 
 const PopularProduct = () => {
   return (
     <div>
-      <h1 className="text-2xl text-center font-semibold ">Loading...</h1>
+      <h1 className="text-2xl text-center font-semibold ">PopularProduct</h1>
     </div>
   );
 };
