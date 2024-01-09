ea0c6e357ad19b7f4a43718a3bf1fc23fc06c3db
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-09 23:55:57 +0600

    root Loading and section loading

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RootLoading = () => {
+  return (
+    <div className="text-center font-semibold text-2xl">
+      <h2>Loading...</h2>
+    </div>
+  );
+};
+
+export default RootLoading;
