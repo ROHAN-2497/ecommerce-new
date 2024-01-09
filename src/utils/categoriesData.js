cbaf88a23382250669796b74be4c4a85652a7d7e
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-09 14:03:56 +0600

    revaidate data

--- src/utils/categoriesData.js
+++ src/utils/categoriesData.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const categoriesData = async () => {
-  const res = await fetch("http://localhost:5000/categories", {
-    cache: "force-cache",
-  });
+const res = await fetch("http://localhost:5000/categories", {next:{
+  revalidate: 5
+}});
   return res.json();
 };
 
