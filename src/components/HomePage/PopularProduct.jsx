bfbcba81d30244ecad9f4556ad1f5bac13789594
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-09 23:58:00 +0600

    updated

---
+++ src/components/HomePage/PopularProduct.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PopularProduct = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-semibold ">Loading...</h1>
+    </div>
+  );
+};
+
+export default PopularProduct;
