53f86c087d0dde17c1ba6a00d9193e8f78fca658
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-11 23:53:43 +0600

    get api modified for e single product Show

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,11 +1,17 @@
 import db from "@/db.json";
 import { request } from "express";
 import { NextResponse } from "next/server";
+import { Result } from "postcss";
 
 export const GET = (request) => {
   // console.log(request.cookies.get(""), request.headers.get(""))
-  const products = db.products;
+  const { searchParams } = new URL(request.url);
+  const categoryId = seearchParams.get("categoryId");
 
+  let products = db.products;
+  if (categoryId) {
+    products = products.filter((product) => product.categoryId == +categoryId);
+  }
   return NextResponse.json(products);
 };
 
