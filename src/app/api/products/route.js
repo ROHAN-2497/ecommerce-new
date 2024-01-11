d0068d350faf81a55fe27cb151fbba374a8716d6
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-11 23:40:45 +0600

    get api

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,9 +1,15 @@
 import db from '@/db.json'
+import { request } from 'express'
 import { NextResponse } from "next/server"
 
 export const GET = (request) =>{
-    // console.log(request.cookies.get(""), request.headers.get(""))
+    // console.log(request.cookies.get(""), request.headers.get(""))        
     const products = db.products
     
     return NextResponse.json(products)
+}
+
+export const POST = (request) =>{
+    const products = db.products
+    return
 }
